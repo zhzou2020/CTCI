a5561f3e89eac35b5f32cdd33b73a4423bd9725e
Zhuojin Zou <zzjstudy@gmail.com>
2016-08-31 20:54:54 +0800

    Create 3_6.cpp

---
+++ Chapter3/3_6.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void sort(stack<int> * s){
+	stack<int> s1;//smallest on the top
+	stack<int> * sorted = &s1;
+	
+	while(!s -> empty()){
+		int tmp = s -> top();
+		s -> pop();
+		while(!sorted -> empty() && tmp > sorted -> top()){
+			s -> push(sorted -> top());
+			sorted -> pop();
+		}
+		sorted -> push(tmp);
+	}
+	
+	while(! sorted -> empty()){
+		s -> push(sorted -> top());
+		sorted -> pop();
+	}
+}
+	
+int main(){
+    stack<int> my;
+    int arr[] = {1, 7, 3, 4};
+    for(int i=0; i<sizeof(arr)/sizeof(arr[0]); i++){
+        my.push(arr[i]);
+    }
+    sort(&my);
+    while(!my.empty()){
+        cout<<my.top()<<'\n';
+        my.pop();
+    }
+}
